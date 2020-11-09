44bd5c4f32bb1dcd346af32750397e7f187117d3
Weronika Jaworek <s18630@pjwstk.edu.pl>
2020-11-09 22:25:02 +0100

    Klasa PlikWejsciowy utworzony. Działa dla 3 przypadków: 1.wyświetla plik domyślny bez parametów 2.wyswietla plik z poprawnym parametrem 3.informuje gdy do pliku dostarczono niepoprawny parametr

---
+++ Cwiczenie2/Cwiczenie2/InputFile.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+namespace Cwiczenie2
+{
+    public class InputFile
+    {
+        public string path { get; set; }
+        public List<string> content { get; }
+        
+
+        public InputFile(string path)
+        {
+
+            setPath(path);
+
+            content = new List<string>();
+
+            readContent();
+
+        }
+
+
+        public InputFile()
+        { 
+
+            setPath(@"dane.csv");
+         
+            content = new List<string>();
+
+            readContent();
+
+        }
+
+  
+        private void readContent(){
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s = "";
+                while ((s = sr.ReadLine()) != null)
+                {
+                    content.Add(s);
+                   
+                }
+            }
+
+
+
+        }
+
+
+        private void setPath( string path)
+        {
+
+
+            if (File.Exists(path))
+            {
+                this.path = path;
+            }
+            else
+            {
+
+                throw new FileNotFoundException("Plik " + path + " nie istnieje");
+            }
+
+
+
+
+        }
+
+
+        public void showContent()
+        {
+
+         foreach(string s in content)
+         {
+           Console.WriteLine(s);
+         }
+
+        }
+
+
+      
+
+        public List<string> getFalseContent()
+        {
+
+            List<string> falseContent = content;
+            falseContent.Add("222, 3,2");
+            falseContent.Add(", 3,2");
+
+            return falseContent;
+        }
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+    
+
+    
+    
+
+
+  
+
+
+    
+
---
+++ Cwiczenie2/Cwiczenie2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace Cwiczenie2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+            try
+            {
+
+
+                    //      InputFile plikBezParametru = new InputFile();
+                   //      plikBezParametru.showContent();
+
+                    //    InputFile plikPoprawnyParametr = new InputFile("C:\\Users\\weron\\OneDrive\\Pulpit\\Plik testowy folder\\danetestowe.csv");
+                    //    plikPoprawnyParametr.showContent();
+
+
+                  //      InputFile plikNiepoprwanyParametr = new InputFile("C:\\weron\\OneDrive\\Pulpit\\Plik testowy folder\\danetestowe.csv");
+                   //      plikNiepoprwanyParametr.showContent();
+
+
+
+
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
