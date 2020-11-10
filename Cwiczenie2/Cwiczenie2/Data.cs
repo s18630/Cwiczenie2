904b2f61b6f0d2066b970f71f226a977d9f8222b
Weronika Jaworek <s18630@pjwstk.edu.pl>
2020-11-10 02:20:30 +0100

    Dodana klasa reorganizująca dane: sprawdza czy jest wymagana liczna kolumn sprawdza czy w kolumnie nie ma pustej wartości
    UWAGA: nie sprawdza:
     czy dodano już do listy zwrotnej te dane
    nie układa danych w odpowiedniej kolejności

---
+++ Cwiczenie2/Cwiczenie2/Data.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Cwiczenie2
+{
+    class Data
+
+    {
+        public List<string> data;
+
+
+        public Data(List<string> lista)
+        {
+            data = new List<string>();
+            extractData(lista);
+
+        }
+
+
+
+
+
+
+
+
+
+        public void extractData(List<string> lista)
+        {
+            int iloscposprawdzeniu = 0;
+            int iloscrekordowWejsciowych = lista.Count;
+
+
+     
+
+
+            foreach (string s in lista)
+            {
+               
+
+                string[] studentsData = s.Split(',');
+
+
+
+                try
+                {
+                    CorrectRecord correctRecord = new CorrectRecord(studentsData);
+                    string record = correctRecord.getCorrectedRecord();
+
+                     // sptawdzić czy się nie powtarzają
+                    data.Add(record);
+
+
+                    correctRecord.showDataSet();
+
+         
+
+                    iloscposprawdzeniu++;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+
+                }
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+            // zrobić set data i dodać 
+
+
+
+            Console.WriteLine(" przed " + iloscrekordowWejsciowych + "----> " + "po" + iloscposprawdzeniu);
+
+
+
+
+        }
+
+
+        public void showData()
+        {
+            if (data.Count == 0)
+            {
+                Console.WriteLine("Lista jest pusta ");
+            }
+
+            else
+            {
+
+                Console.WriteLine(" Dostepne dane:");
+
+                int i = 0;
+
+                foreach (string s in data)
+                { 
+                    i++;
+                    Console.WriteLine(" Zestaw Danych nr " + i);
+
+                    Console.WriteLine(s + "\n");
+                }
+
+                Console.WriteLine("Zostało zapisane " + i + "zestawów danych");
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+        private class CorrectRecord
+
+        {
+            private string[] record;
+            private string correctedRecord;
+
+            public CorrectRecord(string[] record)
+            {
+
+
+                if (record.Length != 9)
+                {
+                    throw new Exception("Zła ilośc dostarczonych do konstruktora danych");
+                }
+
+                if (isBlankFiled(record))
+                {
+
+                    throw new Exception(" Jenda z kolumn nawiera nieprawidłową wartość");
+
+                }
+                else
+                {
+                    this.record = record;
+                    // dodać potem funcke na odpowiednie miejsca
+
+                    correctedRecord= string.Join(",", record);
+                    
+
+                }
+
+               
+
+            }
+
+
+
+            public string getCorrectedRecord()
+            {
+                return correctedRecord;
+            }
+
+
+
+
+            public bool isBlankFiled(string[] fields)
+            {
+                int column = 0;
+
+                foreach (string s in fields)
+                {
+                    column++;
+
+                    if (isItEmpty(s) == true)
+                    {
+                        Console.WriteLine(" Niepasujące pole w kolumnie " + column);
+                        return true;
+
+                    }
+
+                }
+
+                return false;
+            }
+
+
+
+            private bool isItEmpty(string pole)
+            {
+                return !(Regex.IsMatch(pole, "[a-z0-9]+", RegexOptions.IgnoreCase));
+
+            }
+
+
+
+
+            public void showDataSet()
+            {
+                Console.WriteLine();
+                int count = 0;
+
+                foreach (string s in record)
+                {
+                    count++;
+
+                    Console.WriteLine("Kolumna " + count + ": " + s);
+
+                }
+
+
+            }
+
+        }
+    
+
+
+        }
+    }
+
--- Cwiczenie2/Cwiczenie2/Program.cs
+++ Cwiczenie2/Cwiczenie2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace Cwiczenie2
@@ -15,15 +16,27 @@ namespace Cwiczenie2
             {
 
 
-                    //      InputFile plikBezParametru = new InputFile();
-                   //      plikBezParametru.showContent();
+                         InputFile plikBezParametru = new InputFile();
+                         plikBezParametru.showContent();
 
-                    //    InputFile plikPoprawnyParametr = new InputFile("C:\\Users\\weron\\OneDrive\\Pulpit\\Plik testowy folder\\danetestowe.csv");
-                    //    plikPoprawnyParametr.showContent();
+                //    InputFile plikPoprawnyParametr = new InputFile("C:\\Users\\weron\\OneDrive\\Pulpit\\Plik testowy folder\\danetestowe.csv");
+                //    plikPoprawnyParametr.showContent();
 
 
-                  //      InputFile plikNiepoprwanyParametr = new InputFile("C:\\weron\\OneDrive\\Pulpit\\Plik testowy folder\\danetestowe.csv");
-                   //      plikNiepoprwanyParametr.showContent();
+                //      InputFile plikNiepoprwanyParametr = new InputFile("C:\\weron\\OneDrive\\Pulpit\\Plik testowy folder\\danetestowe.csv");
+                //      plikNiepoprwanyParametr.showContent();
+
+
+
+
+                Data daneStudentow = new Data(plikBezParametru.content);
+                daneStudentow.showData();
+                                
+                            
+                                
+                                
+
+
 
 
 
@@ -37,7 +50,7 @@ namespace Cwiczenie2
 
 
 
-
+           
 
 
 
